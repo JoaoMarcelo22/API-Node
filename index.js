efde829ad47ce00ceb6531b9e5b21d6d6b4208ca
Joao Marcelo <joao_marcelo.22@hotmail.com>
2022-06-21 18:03:25 -0300

    feat: :sparkles: Primeira Api usando o Node, e interagindo com json

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+const produtos = require("./src/produtos/produtos.json")
+
+app.get("/produtos",(req,res) => {
+    return res.json(produtos);
+})
+
+app.listen(port,() =>{
+    console.log("Running serv");
+})
